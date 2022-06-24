d671f216254cfc1ec93d8a9c7eb0f8a6f21f5fbf
lauracontreras12 <107431344+lauracontreras12@users.noreply.github.com>
2022-06-24 11:25:52 -0700

    Update util.py
    added arguments

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 def nth_power(n, power):
-    '''calculates power for number upto n'''
+    '''calculates power for number upto n
+    args:
+        n: highest number in the list of numbers
+        power: power for numbers to false, default is 2'''
     return [i**power for i in range(n)]
 
 
