a0b4e567199b2622e4ff0e32cae582866c722655
Laura Contreras <laura.contreras@du.edu>
2022-06-24 11:32:07 -0700

    added lambda for action on numbers

--- util.py
+++ util.py
@@ -1,6 +1,9 @@
-def nth_power(n, power):
-    '''calculates power for number upto n'''
-    return [i**power for i in range(n)]
+def nth_power(n, fn = lambda x: x**2):
+    '''calculates power for number upto n
+    args:
+        n: highest number in the list of numbers
+        power: power for numbers to false, default is 2'''
+    return [fn(i) for i in range(n)]
 
 
-print(nth_power(10, 4))
+print(nth_power(10))
