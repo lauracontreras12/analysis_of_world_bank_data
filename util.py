f3aa0c9ca3d8596ac3bbc0c1b4c5097928301f0f
Laura Contreras <laura.contreras@du.edu>
2022-06-24 11:54:27 -0700

    updated default power

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
-def nth_power(n, fn = lambda x: x**2):
+def nth_power(n, fn = lambda x: x**3):
     '''calculates power for number upto n
     args:
         n: highest number in the list of numbers
-        power: power for numbers to false, default is 2'''
+        power: power for numbers to false, default is 3'''
     return [fn(i) for i in range(n)]
 
 
